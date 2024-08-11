55ee3475961e069d6d210f884ff5ee7bb421e7b7
Wladimir Guevara Berbesi <wladige@gmail.com>
2024-08-11 18:45:20 -0400

    feat: se agrega test criterio 4 de la pueba

--- backend/tests/server.spec.js
+++ backend/tests/server.spec.js
@@ -17,5 +17,28 @@ it('should return 404 when trying to delete a non-existent cafe', async () => {
     expect(response.statusCode).toBe(404);
     expect(response.body).toEqual({ message: "No se encontró ningún cafe con ese id" });
 });
-
+it('should add a new cafe and return 201', async () => {
+    const newCafe = {
+      id: 5,
+      nombre: 'Latte'
+    };
+    const response = await request(server)
+      .post('/cafes')
+      .send(newCafe);
+    expect(response.statusCode).toBe(201);
+    expect(response.body).toEqual(expect.arrayContaining([newCafe]));
+  });
+  it('should return 400 when trying to update a cafe with different id in params and payload', async () => {
+    const cafeToUpdate = {
+      id: 4,
+      nombre: 'Latte'
+    };
+    const response = await request(server)
+      .put('/cafes/6') // id en params es diferente al id en payload
+      .send(cafeToUpdate);
+    expect(response.statusCode).toBe(400);
+    expect(response.body).toEqual({
+      message: "El id del parámetro no coincide con el id del café recibido"
+    });
+  });
 });
