51e522afac4e860a1fbcb9541642282fdf12056c
Wladimir Guevara Berbesi <wladige@gmail.com>
2024-08-11 18:36:56 -0400

    feat: se agrega test que devuelva 404 y se refactoriza ruta GET

--- backend/tests/server.spec.js
+++ backend/tests/server.spec.js
@@ -2,26 +2,20 @@ const request = require("supertest");
 const server = require("../index");
 
 describe("Operaciones CRUD de cafes", () => {
-it('should test GET /cafes', async () => {
-    const response = await request(server).get('/cafes');
-    expect(response.statusCode).toBe(200);
-    expect(response.body).toEqual([
-        {
-            id: 1,
-            nombre: 'Cortado'
-        },
-        {
-            id: 2,
-            nombre: 'Americano'
-        },
-        {
-            id: 3,
-            nombre: 'Mocacino'
-        },
-        {
-            id: 4,
-            nombre: 'Cappuccino'
-        }
-    ]);
-})
+    it('should return 200 and an array of cafes', async () => {
+        const response = await request(server).get('/cafes');
+        expect(response.statusCode).toBe(200);
+        expect(response.body).toBeInstanceOf(Array);
+        expect(response.body.length).toBeGreaterThan(0);
+        expect(response.body[0]).toBeInstanceOf(Object);
+    });
+it('should return 404 when trying to delete a non-existent cafe', async () => {
+    const token = 'tu-token-de-autorizacion';
+    const response = await request(server)
+        .delete('/cafes/1000')
+        .set("Authorization", token);
+    expect(response.statusCode).toBe(404);
+    expect(response.body).toEqual({ message: "No se encontró ningún cafe con ese id" });
+});
+
 });
